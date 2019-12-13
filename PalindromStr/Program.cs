aab49a21b266098674b4db9d4fff25a467100584
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-12-13 23:38:44 +0300

    Добавьте файлы проекта.

---
+++ PalindromStr/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromStr {
+    class Program {
+        static void Main() {
+            string sStr = Console.ReadLine();
+            // Реверсирование строки
+            string reverseStr = new string(sStr.ToCharArray().Reverse().ToArray());
+            // Проверяет строку на равенство
+            if (reverseStr.Equals(sStr)) {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else {
+                Console.WriteLine("Строка не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
